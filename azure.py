655549a85babacbbf21c344fdefc4bd862dd21eb
yshk-mrt <34604520+yshk-mrt@users.noreply.github.com>
2023-10-16 17:14:06 -0400

    temperature update

--- azure.py
+++ azure.py
@@ -7,8 +7,8 @@ import os
 openai.api_key = os.environ.get('OPENAI_API_KEY')
 # Initialize Streamlit
 st.title("Awesome Chatbot Test")
-temp = st.slider('What is the temparature(Celsius)?', -20, 50, 20)
-st.write("Temparature is ", temp, ' Celsius')
+temp = st.slider('Select the temperature', 0.0, 0.7, 1.5)
+st.write("Temperature is ", temp)
 # Create a text input field for user queries
 user_input = st.text_input("Ask any question:")
 # Send the user's query to OpenAI GPT-3
@@ -16,6 +16,7 @@ if user_input:
     response = openai.Completion.create(
     engine="text-davinci-003",
     prompt=user_input,
-    max_tokens=50
+    max_tokens=50,
+    temperature=temp
     )
     st.write(response['choices'][0]['text'].strip())
