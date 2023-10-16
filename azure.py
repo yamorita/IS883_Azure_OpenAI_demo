85166341f4102924cb37b1493bf12303247a398b
yshk-mrt <34604520+yshk-mrt@users.noreply.github.com>
2023-10-16 16:38:31 -0400

    Update input guidance

--- azure.py
+++ azure.py
@@ -8,7 +8,7 @@ openai.api_key = os.environ.get('OPENAI_API_KEY')
 # Initialize Streamlit
 st.title("Awesome Chatbot")
 # Create a text input field for user queries
-user_input = st.text_input("Ask a question:")
+user_input = st.text_input("Ask any question:")
 # Send the user's query to OpenAI GPT-3
 if user_input:
     response = openai.Completion.create(
