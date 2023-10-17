68199c765837074b610bace8500ea6786a0a97ee
yshk-mrt <34604520+yshk-mrt@users.noreply.github.com>
2023-10-17 10:33:12 -0400

    Add temperature

--- azure.py
+++ azure.py
@@ -7,7 +7,9 @@ import os
 # Set up your OpenAI API key
 openai.api_key = os.environ.get('OPENAI_API_KEY')
 # Initialize Streamlit
-st.title("Azure Guide Chatbot")
+st.title("Awesome Chatbot")
+temperature = st.slider('Select the temperature', 0.0, 0.7, 1.5)
+st.write("Temperature is ", temperature)
 # Create a text input field for user queries
 user_input = st.text_input("Ask a question:")
 # Send the user's query to OpenAI GPT-3
@@ -15,6 +17,7 @@ if user_input:
     response = openai.Completion.create(
     engine="text-davinci-003",
     prompt=user_input,
-    max_tokens=50
+    max_tokens=50,
+    temperature=temperature
     )
     st.write(response['choices'][0]['text'].strip())
